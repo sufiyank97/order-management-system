6eae7bc0e2bfe5fe59502c0b3ba82acfb7bc36a3
dubeyrahul508 <dubeyrahul508@gmail.com>
2020-07-19 18:23:16 +0530

    added build

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ app.use(express.json())
 app.use(cors())
 const path = require('path')
 app.use('/api', router)
-app.use(express.static(path.join(__dirname, "client/build")))
+app.use(express.static(path.join(__dirname, "client/src")))
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    res.sendFile(path.join(__dirname + "/client/src/index.html"))
 })
 
 app.listen(port, function () {
